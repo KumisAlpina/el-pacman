a84d128a5d9d35ba12b83a5fc1ae8cd6b5a52148
Ángel Sánchez-wq <astivensanchez@ucundinamarca.edu.co>
2025-05-10 22:46:32 -0500

    se agregaron fantasmas con sus respectiva mecanica de game over y movimiento aleatorio

--- game.py
+++ game.py
@@ -16,11 +16,18 @@ class Game:
 
         #bucle principal
         self.running = True
+        self.game_state = PLAYING
 
-        #crear el jugador
+        #crear el jugador, paredes y monedas(fantasmas)
         self.walls = []
+        self.coins = []
+        self.score = 0
         self.player = None
+        self.ghosts = []
         self.create_level()
+
+        #Fuente para el texto
+        self.font = pygame.font.Font(None, 36)
 
     def create_level(self):
         """Crear el nivel a partir de la matriz LEVEL"""
@@ -29,8 +36,18 @@ class Game:
                 if cell == "1":
                     # Crear una pared
                     self.walls.append(Wall(col_index, row_index))
+                elif cell == "0":
+                    self.coins.append(Coin(col_index, row_index))
                 elif cell == "P":
                     self.player = Player(col_index, row_index)
+                elif cell == "R":
+                    self.ghosts.append(Ghost(col_index, row_index, 'red'))
+                elif cell == "B":
+                    self.ghosts.append(Ghost(col_index, row_index, 'blue'))
+                elif cell == "O":
+                    self.ghosts.append(Ghost(col_index, row_index, 'orange'))
+                elif cell == "G":
+                    self.ghosts.append(Ghost(col_index, row_index, 'green'))
 
     def handle_events(self):
         #manejar eventos
@@ -38,11 +55,29 @@ class Game:
             #si cierra la ventana
             if event.type == pygame.QUIT:
                 self.running = False
+        if self.game_state == GAME_OVER:
+            self.running = False
    
     def update(self):
         #actualizar estado del juego
         #mover el jugador
-        self.player.update(self.walls)
+        if self.game_state == PLAYING:
+            self.player.update(self.walls)
+
+            #actualizar fantasmas
+            for ghost in self.ghosts:
+                ghost.update(self.walls)
+                if self.player.rect.colliderect(ghost.rect):
+                    self.game_state = GAME_OVER
+
+        #actualizar monedas y comprobar coliciones
+        for coin in self.coins[:]: #usar una copia de la lista para modificarla
+            coin.update()
+            if self.player.rect.colliderect(coin.rect):
+                self.coins.remove(coin)
+                self.score += POINTS_PER_COIN
+
+        
 
     def draw(self):
         #dibujar en la pantalla
@@ -53,10 +88,20 @@ class Game:
         #dibujar paredes
         for wall in self.walls:
             wall.draw(self.screen)
+        
+        #dibujar las monedas
+        for coin in self.coins:
+            coin.draw(self.screen)
        
         #dibujar el jugador
-        
         self.player.draw(self.screen)
+        
+        for ghost in self.ghosts:
+            ghost.draw(self.screen)
+
+        #dibujar el puntaje
+        score_text = self.font.render(f'Puntaje: {self.score}', True, WHITE)
+        self.screen.blit(score_text, (10, 10))
         
         #Actualizar la pantalla
         pygame.display.flip()
--- sprites.py
+++ sprites.py
@@ -1,5 +1,6 @@
 import pygame
 from config import *
+import random
 
 class Wall:
     def __init__(self, x, y):
@@ -8,6 +9,186 @@ class Wall:
     def draw(self, screen):
         #dibujar pared en pantalla
         pygame.draw.rect(screen, WALL_COLOR, self.rect)
+    
+class Coin:
+    def __init__(self, x, y):
+        #pocicion monedas
+        self.x = x * TILE_SIZE + TILE_SIZE // 2
+        self.y = y * TILE_SIZE + TILE_SIZE // 2
+
+        #Cargar sprite sheet de la moneda
+        self.sprite_sheet = load_image('BigCoin.png')
+
+        #Cargar frames de animacion
+        self.frames = []
+        for i in range(COIN_FRAMES):
+            frame = pygame.Surface((16, 16), pygame.SRCALPHA)
+            frame.blit(self.sprite_sheet, (0, 0), (i * 16, 0, 16, 16))
+            frame = pygame.transform.scale(frame, (COIN_SIZE, COIN_SIZE))
+            self. frames.append(frame)
+        #variables de animaciion
+        self.current_frame = 0
+        self.animation_timer = pygame.time.get_ticks()
+
+        #rectangulo de colisiones
+        self.rect = self.frames[0].get_rect(center=(self.x, self.y))
+
+    def update(self):
+        """Actualizar animacion de la moneda"""
+        current_time = pygame.time.get_ticks()
+        if current_time - self.animation_timer > COIN_ANIMATION_SPEED:
+            self.current_frame = (self.current_frame + 1) % COIN_FRAMES
+            self.animation_timer = current_time
+
+    def draw(self, screen):
+        """dibujar moneda en pantalla"""
+        screen.blit(self.frames[self.current_frame], self.rect)
+
+
+class Ghost:
+    def __init__(self, x, y, ghost_type):
+        #posicion inicial del fantasma
+        self.x = x * TILE_SIZE + TILE_SIZE // 2
+        self.y = y * TILE_SIZE + TILE_SIZE // 2
+
+        # Tipo de fantasmas y sus caracteristicas
+        self.ghost_type = ghost_type
+        self.speed = GHOST_SPEEDS[ghost_type]
+        self.direction_change_time = GHOST_DIRECTION_TIMES[ghost_type]
+
+        #cargar sprite del fantasma
+        self.sprite_sheet = load_image(GHOST_SPRITES[ghost_type])
+
+        #cargar frames de animacion
+        self.frames = []
+        for i in range(GHOST_ANIMATION_FRAMES):
+            frame = pygame.Surface((16, 16), pygame.SRCALPHA)
+            frame.blit(self.sprite_sheet, (0, 0), (i * 16, 0, 16, 16))
+            frame = pygame.transform.scale(frame, (GHOST_SIZE, GHOST_SIZE))
+            self.frames.append(frame)
+        #variables de animacon
+        self.current_frame = 0
+        self.animation_timer = pygame.time.get_ticks()
+
+        #variables de movimiento
+        self.direction = random.randint(0, 3)
+        self.direction_timer = pygame.time.get_ticks()
+        self.rect = self.frames[0].get_rect(center=(self.x, self.y))
+    
+    def get_next_direction(self):
+        """Determinar la siguiente direccion del fantasma"""
+        if self.ghost_type == 'red':
+            # el fantasma rojo elije una direccion aleatoria
+            return random.randint(0, 3)
+        elif self.ghost_type == 'blue':
+            # el fantasma azul alterna entre vertical y horizontal
+            if self.direction in [RIGHT, LEFT]:
+                return random.choice([UP, DOWN])
+            else:
+                return random.choice([RIGHT, LEFT])
+        elif self.ghost_type == 'orange':
+            # el fantasma naranja se mueve en sentido horario
+            return (self.direction + 1) % 4
+        else:
+            #el fantasma verde se mueve en sentido antihorario
+            return (self.direction - 1) % 4
+    
+    def change_direcction(self, walls):
+        """Cambiar la direccion del fantasma segun su tipo"""
+        if self.ghost_type == 'red':
+            # el rojo prueba todas las direcciones hasta obtener una valida
+            directions = list(range(4))
+            random.shuffle(directions)
+            for new_dir in directions:
+                if self.can_move_in_direction(new_dir, walls):
+                    self.direction = new_dir
+                    break
+        
+        else:
+            # los demas sigeun su patron especifico
+            new_dir = self.get_next_direction()
+            if self.can_move_in_direction(new_dir, walls):
+                self.direction = new_dir
+            else:
+                # si no pueden moverse en una direccion se mueven en una aleatoria
+                self.direction = random.randint(0, 3)
+        self.direction_timer = pygame.time.get_ticks()
+
+    def can_move_in_direction(self, direction, walls):
+        """comprobar si el fantasma puede moverse"""
+        dx = dy = 0 
+        if direction == RIGHT:
+            dx = self.speed
+        elif direction == LEFT:
+            dx = -self.speed
+        elif direction == UP:
+            dy = -self.speed
+        elif direction == DOWN:
+            dy = self.speed
+
+        test_rect = self.rect.copy()
+        test_rect.x += dx
+        test_rect.y += dy
+
+        for wall in walls:
+            if test_rect.colliderect(wall.rect):
+                return False
+            return True
+
+    def move(self, walls):
+        """mover fantasmas y manejar coliciones"""
+        current_time = pygame.time.get_ticks()
+        if current_time - self.direction_timer > self.direction_change_time:
+            self.change_direcction(walls)
+
+        # calcular movimiento de la direccion
+        dx = dy = 0
+        # calcular mov segun la direccion
+        dx = dy = 0
+        if self.direction == RIGHT:
+            dx = self.speed
+        elif self.direction == LEFT:
+            dx = -self.speed
+        elif self.direction == UP:
+            dy = -self.speed
+        elif self.speed == DOWN:
+            dy = self.speed
+        
+        # comprobar colicion en la nueva posicion
+        new_rect = self.rect.copy()
+        new_rect.x += dx
+        new_rect.y += dy
+
+        # si hay colision cambiar de direccion
+        can_move = True
+        for wall in walls:
+            if new_rect.colliderect(wall.rect):
+                can_move = False
+                self.change_direcction(walls)
+                break
+        # si no hay colicion actualizar posicion
+        if can_move:
+            self.x += dx
+            self.y += dy
+            self.rect.center = (self.x, self.y)
+            
+        
+
+    def update(self, walls):
+        """Actualizar el estado del fantasma"""
+        # Actualizar animacion
+        current_time = pygame.time.get_ticks()
+        if current_time - self.animation_timer > GHOST_ANIMATION_SPEED:
+            self.current_frame = (self.current_frame + 1) % GHOST_ANIMATION_FRAMES
+            self.animation_timer = current_time
+
+        # Actualizar el movimiento
+        self.move(walls)
+
+    def draw(self, screen):
+        """Dibujar el fantasma en la pantalla"""
+        screen.blit(self.frames[self.current_frame], self.rect)
+
 
 class Player:
     def __init__(self, x, y):
@@ -83,41 +264,34 @@ class Player:
             
 
     def move(self, walls):
-        """Mover a el jugador segun la entrada del usuario"""
-        #intentar mov en x
+        """Mover al jugador según la entrada del usuario"""
+        # Intentar movimiento en x
         if self.dx != 0:
-            #comprobar colicion en x
-            if not self.check_collision(walls, self.dx, 0):
+            # Crear un rectángulo temporal para la nueva posición en x
+            future_rect = self.rect.copy()
+            future_rect.x += self.dx
+            # Comprobar colisión en x
+            if not any(future_rect.colliderect(wall.rect) for wall in walls):
                 self.x += self.dx
-            else:
-                #intentar deslizarse verticalmente si hay colicion
-                if self.check_collision(walls, self.dx, -SLIDE_SPEED):
-                    if not self.check_collision(walls, self.dx, SLIDE_SPEED):
-                        self.y += SLIDE_SPEED
-                    else:
-                        self.y -= SLIDE_SPEED
-        #intentar movimiento en y
+
+        # Intentar movimiento en y
         if self.dy != 0:
-            #comprobar colicion en y
-            if not self.check_collision(walls, 0, self.dy):
+            # Crear un rectángulo temporal para la nueva posición en y
+            future_rect = self.rect.copy()
+            future_rect.y += self.dy
+            # Comprobar colisión en y
+            if not any(future_rect.colliderect(wall.rect) for wall in walls):
                 self.y += self.dy
-            else:
-                #intentar deslizarse horizontalmente si hay colicion
-                if self.check_collision(walls, -SLIDE_SPEED, self.dy):
-                    if not self.check_collision(walls, SLIDE_SPEED, self.dy):
-                        self.x += SLIDE_SPEED
-                    else:
-                        self.x -= SLIDE_SPEED
-        #actualizar el rectangulo a la nueva posicion
+
+        # Actualizar el rectángulo a la nueva posición
         self.rect.center = (self.x, self.y)
 
-
-        #Mantener el jugador dentro de la pantalla
+         # Mantener al jugador dentro de la pantalla
         if self.x > SCREEN_WIDTH - PLAYER_SIZE:
             self.x = 0
         elif self.x < 0:
             self.x = SCREEN_WIDTH - PLAYER_SIZE
-        
+
         if self.y > SCREEN_HEIGHT - PLAYER_SIZE:
             self.y = 0
         elif self.y < 0:
